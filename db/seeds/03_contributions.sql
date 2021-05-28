1b7d061b4a6e9eb00ffe131b218675c3bda9e9f8
Zobee <zohebboga@hotmail.com>
2021-05-28 23:38:29 +0000

    Added seed data

--- db/seeds/03_contributions.sql
+++ db/seeds/03_contributions.sql
@@ -1,6 +1,6 @@
 -- contributions table seeds here
-INSERT INTO contributions (user_id, story_id, content) VALUES (2, 1, "Once upon a time...");
-INSERT INTO contributions (user_id, story_id, content) VALUES (2, 2, "In a land far away...");
-INSERT INTO contributions (user_id, story_id, content) VALUES (3, 2, "Somewhere in Kentucky...");
-INSERT INTO contributions (user_id, story_id, content) VALUES (5, 4, "The end");
+INSERT INTO contributions (user_id, story_id, content, is_accepted) VALUES (1, 1, "Once upon a time...", 'accepted');
+INSERT INTO contributions (user_id, story_id, content, is_accepted) VALUES (2, 2, "In a land far away...", 'accepted');
+INSERT INTO contributions (user_id, story_id, content, is_accepted) VALUES (3, 3, "Somewhere in Kentucky...", 'accepted');
+INSERT INTO contributions (user_id, story_id, content) VALUES (5, 2, "The end");
 INSERT INTO contributions (user_id, story_id, content) VALUES (3, 1, "[something offensive that's definitely getting rejected]");
