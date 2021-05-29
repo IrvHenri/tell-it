56ae52cbb28e02e842a4ec4fd04ec8be1c60b430
Irving Henriquez <henriquezirv@gmail.com>
2021-05-29 19:14:23 +0000

    added cookie-session as dependency and created stories.js

---
+++ routes/stories.js
@@ -0,0 +1,43 @@
+/*
+ * All routes for Widgets are defined here
+ * Since this file is loaded in server.js into api/widgets,
+ *   these routes are mounted onto /widgets
+ * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
+ */
+
+const express = require("express");
+const router = express.Router();
+
+module.exports = (db) => {
+  // GET homepage
+  router.get("/", (req, res) => {
+    let query = `SELECT stories.* , users.*
+    FROM stories
+    JOIN users ON stories.user_id = users.id;`;
+    db.query(query)
+      .then((data) => {
+        console.log(data);
+        const stories = data.rows;
+        res.json({ stories });
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
+      });
+  });
+
+  // router.post("/", (req, res) => {
+  //   let user_id = req.session;
+  //   let query = `'INSERT INTO stories (user_id, title,initial_content) VALUES ($1, $2)`;
+  //   let values = [1];
+  //   db.query(query)
+  //     .then((data) => {
+  //       console.log(data);
+  //       const stories = data.rows;
+  //       res.json({ stories });
+  //     })
+  //     .catch((err) => {
+  //       res.status(500).json({ error: err.message });
+  //     });
+  // });
+  return router;
+};
