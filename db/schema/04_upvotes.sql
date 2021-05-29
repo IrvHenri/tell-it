2595ce8259ba763d29ebc4b38f7a4d7b80e50078
Zobee <zohebboga@hotmail.com>
2021-05-29 14:58:06 +0000

    Updated seeds and schema to fix errors with double-quotes

--- db/schema/04_upvotes.sql
+++ db/schema/04_upvotes.sql
@@ -3,5 +3,5 @@ DROP TABLE IF EXISTS upvotes CASCADE;
 CREATE TABLE upvotes (
   id SERIAL PRIMARY KEY NOT NULL,
   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  contribution_id INTEGER REFERENCES contributions(id) ON DELETE CASCADE,
+  contribution_id INTEGER REFERENCES contributions(id) ON DELETE CASCADE
 );
