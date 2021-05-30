f7d47aa6def92654e28f6e32b9862fef347a4333
Zobee <zohebboga@hotmail.com>
2021-05-30 15:51:34 +0000

    moved createStory and render stories to helpers.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -17,38 +17,6 @@ $(() => {
       .catch((err) => console.log(err));
   });
 
-  const renderStories = (stories) => {
-    stories.forEach((story) => {
-      $("#home").prepend(createStory(story));
-      //Event listener for 'view more stories'
-      $('.story-article footer button').on('click', function() {
-        console.log(story);
-      })
-    });
-  };
-
-  const createStory = (story) => {
-    const { title, avatar, initial_content, created_at, username } = story;
-    const safeTitle = escape(title);
-    const safeContent = escape(initial_content);
-    const $story = $(`
-    <article class ='story-article'>
-    <header>
-    <h3> ${username}</h3>
-     <div> <img src = ${avatar} alt= 'avatar' class = 'avatar'> </div>
-    </header>
-    <h2>${safeTitle}</h2>
-    <p class ='story-content'> ${safeContent}</p>
-
-    <footer> <small>${timeago.format(
-      created_at
-    )} </small> <button> View Story </button> </footer>
-    </article>
-
-    `);
-    return $story;
-  };
-
   // $("#story-page").on("click", () => {
   //   $.ajax({
   //     method: "GET",
--- public/scripts/helpers.js
+++ public/scripts/helpers.js
@@ -1,7 +1,37 @@
-console.log('HELPERS')
-
 const escape = function (str) {
   let div = document.createElement("div");
   div.appendChild(document.createTextNode(str));
   return div.innerHTML;
+};
+
+const createStory = (story) => {
+  const { title, avatar, initial_content, created_at, username } = story;
+  const safeTitle = escape(title);
+  const safeContent = escape(initial_content);
+  const $story = $(`
+  <article class ='story-article'>
+  <header>
+  <h3> ${username}</h3>
+   <div> <img src = ${avatar} alt= 'avatar' class = 'avatar'> </div>
+  </header>
+  <h2>${safeTitle}</h2>
+  <p class ='story-content'> ${safeContent}</p>
+
+  <footer> <small>${timeago.format(
+    created_at
+  )} </small> <button> View Story </button> </footer>
+  </article>
+
+  `);
+  return $story;
+};
+
+const renderStories = (stories) => {
+  stories.forEach((story) => {
+    $("#home").prepend(createStory(story));
+    //Event listener for 'view more stories'
+    $('.story-article footer button').on('click', function() {
+      console.log(story);
+    })
+  });
 };
