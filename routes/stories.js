25a083df5ae0721f36a87e944bece338568a9827
Zobee <zohebboga@hotmail.com>
2021-05-30 19:02:50 +0000

    minor tweak to /stories

--- routes/stories.js
+++ routes/stories.js
@@ -1,12 +1,4 @@
-/*
- * All routes for Widgets are defined here
- * Since this file is loaded in server.js into api/widgets,
- *   these routes are mounted onto /widgets
- * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
- */
-
 const express = require("express");
-const { Pool } = require("pg");
 const router = express.Router();
 
 module.exports = (db) => {
