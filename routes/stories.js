e777c1cda597b3f87480d6e39c77b438b095f1fd
Irving Henriquez <henriquezirv@gmail.com>
2021-06-01 05:27:06 +0000

    changed /:storyId route to include contributor avatar

--- routes/stories.js
+++ routes/stories.js
@@ -33,12 +33,13 @@ module.exports = (db) => {
         retObj.story = data.rows[0];
         db.query(
           `
-          SELECT contributions.*, users.username
+          SELECT contributions.*, users.username , users.avatar 
           FROM contributions
           JOIN users ON users.id = user_id
           WHERE story_id = $1
           ORDER BY created_at;
-          `,[req.params.storyId]
+          `,
+          [req.params.storyId]
         )
           .then((data) => {
             retObj.contributions = data.rows;
