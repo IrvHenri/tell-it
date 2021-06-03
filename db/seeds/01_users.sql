d657b2c6e743f0316c065a0a719b74e58d45a7fd
Irving Henriquez <henriquezirv@gmail.com>
2021-06-03 20:22:00 +0000

    changed default avatars

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,6 +1,6 @@
 -- Users table seeds here
-INSERT INTO users (username) VALUES ('Alice');
-INSERT INTO users (username) VALUES ('Bob');
-INSERT INTO users (username) VALUES ('Carol');
+INSERT INTO users (username,avatar) VALUES ('Alice','https://user-images.githubusercontent.com/69181038/120706804-8a9b6180-c487-11eb-923a-ec314ef09395.png');
+INSERT INTO users (username,avatar) VALUES ('Bob','https://user-images.githubusercontent.com/69181038/120706161-c124ac80-c486-11eb-8ca0-456a126b2eec.png');
+INSERT INTO users (username,avatar) VALUES ('Carol','https://user-images.githubusercontent.com/69181038/120706223-d3064f80-c486-11eb-95f7-1beeb33cabaf.png');
 INSERT INTO users (username) VALUES ('Dan');
 INSERT INTO users (username) VALUES ('Emily');
