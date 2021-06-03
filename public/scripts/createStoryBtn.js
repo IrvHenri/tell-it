f817583bb2196733dcdac1aa865770e44d100982
Irving Henriquez <henriquezirv@gmail.com>
2021-06-03 18:22:08 +0000

    fixed create story a tag

--- public/scripts/createStoryBtn.js
+++ public/scripts/createStoryBtn.js
@@ -1,8 +1,9 @@
 $(() => {
-  const user = localStorage.getItem('user_id')
-  user && $(".create-story-btn-container").html(`
-  <button class="create-story-btn">
-    <a href="#ex1" rel="modal:open"><i class="fas fa-pencil-alt"></i></a>
-  </button>
-  `)
-})
+  const user = localStorage.getItem("user_id");
+  user &&
+    $(".create-story-btn-container").html(`
+
+    <a href="#ex1" rel="modal:open" class="create-story-btn"><i class="fas fa-pencil-alt"></i></a>
+
+  `);
+});
