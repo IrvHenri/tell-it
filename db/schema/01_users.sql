ace53a85aa47764626e345cd02b066cb3cf9e1c0
Irving Henriquez <henriquezirv@gmail.com>
2021-06-03 20:52:51 +0000

    minor change to default avatar

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -3,5 +3,5 @@ DROP TABLE IF EXISTS users CASCADE;
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
   username VARCHAR(255) NOT NULL,
-  avatar VARCHAR(255) NOT NULL DEFAULT 'https://cdn.pixabay.com/photo/2014/04/02/17/07/user-307993_960_720.png'
+  avatar VARCHAR(255) NOT NULL DEFAULT 'https://user-images.githubusercontent.com/69181038/120706223-d3064f80-c486-11eb-95f7-1beeb33cabaf.png'
 );
