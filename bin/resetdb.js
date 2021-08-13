5c35728a905e48d4bae537d440093d0c2c28a684
Irving Henriquez <henriquezirv@gmail.com>
2021-08-13 13:17:57 -0400

    fixed ssl issue in db.js

--- bin/resetdb.js
+++ bin/resetdb.js
@@ -1,23 +1,24 @@
 // load .env data into process.env
-require('dotenv').config();
+require("dotenv").config();
 
 // other dependencies
-const fs = require('fs');
-const chalk = require('chalk');
-const Client = require('pg-native');
+const fs = require("fs");
+const chalk = require("chalk");
+const Client = require("pg-native");
 
 // PG connection setup
-const connectionString = process.env.DATABASE_URL ||
-  `postgresql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?sslmode=disable`;
+const connectionString =
+  process.env.DATABASE_URL ||
+  `postgresql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?sslmode=require`;
 const client = new Client();
 
 // Loads the schema files from db/schema
 const runSchemaFiles = function () {
   console.log(chalk.cyan(`-> Loading Schema Files ...`));
-  const schemaFilenames = fs.readdirSync('./db/schema');
+  const schemaFilenames = fs.readdirSync("./db/schema");
 
   for (const fn of schemaFilenames) {
-    const sql = fs.readFileSync(`./db/schema/${fn}`, 'utf8');
+    const sql = fs.readFileSync(`./db/schema/${fn}`, "utf8");
     console.log(`\t-> Running ${chalk.green(fn)}`);
     client.querySync(sql);
   }
@@ -25,10 +26,10 @@ const runSchemaFiles = function () {
 
 const runSeedFiles = function () {
   console.log(chalk.cyan(`-> Loading Seeds ...`));
-  const schemaFilenames = fs.readdirSync('./db/seeds');
+  const schemaFilenames = fs.readdirSync("./db/seeds");
 
   for (const fn of schemaFilenames) {
-    const sql = fs.readFileSync(`./db/seeds/${fn}`, 'utf8');
+    const sql = fs.readFileSync(`./db/seeds/${fn}`, "utf8");
     console.log(`\t-> Running ${chalk.green(fn)}`);
     client.querySync(sql);
   }
@@ -44,5 +45,3 @@ try {
   console.error(chalk.red(`Failed due to error: ${err}`));
   client.end();
 }
-
-
